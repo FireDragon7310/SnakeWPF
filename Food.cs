c857a5c06e6a67da64db0a76889fac17baf72486
piros.szabolcs <piros.szabolcs@stud.uni-obuda.hu>
2024-04-27 21:49:43 +0200

    Separating methods for the Food class into Food.cs file

---
+++ Food.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows;
+
+namespace SnakeWPF
+{
+    public class Food
+    {
+        private readonly Canvas gameCanvas;
+        readonly Random random = new Random();
+        private readonly int foodSize = 20;
+        private Ellipse foodPiece;
+        readonly List<Point> snakeSegments;
+
+        public Food(Canvas canvas, List<Point> snakeSegments)
+        {
+            gameCanvas = canvas;
+            this.snakeSegments = snakeSegments;
+            GenerateFood();
+        }
+
+        private void GenerateFood()
+        {
+            double left = random.Next(0, (int)(gameCanvas.ActualWidth / foodSize)) * foodSize;
+            double top = random.Next(0, (int)(gameCanvas.ActualHeight / foodSize)) * foodSize;
+
+
+            foodPiece = new Ellipse
+            {
+                Width = foodSize,
+                Height = foodSize,
+                Fill = Brushes.Red
+            };
+
+            gameCanvas.Children.Add(foodPiece);
+            Canvas.SetLeft(foodPiece, left);
+            Canvas.SetTop(foodPiece, top);
+        }
+
+        public bool Eat(Point snakeHeadPosition)
+        {
+            double foodLeft = Canvas.GetLeft(foodPiece);
+            double foodTop = Canvas.GetTop(foodPiece);
+
+            if (snakeHeadPosition.X == foodLeft && snakeHeadPosition.Y == foodTop)
+            {
+                gameCanvas.Children.Remove(foodPiece);
+                GenerateFood();
+
+                Point newSegmentPositon = new Point(snakeHeadPosition.X, snakeHeadPosition.Y);
+                snakeSegments.Add(newSegmentPositon);
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace SnakeWPF
         private int dy = 0;
         private DispatcherTimer gameTimer;
         private Food food;
-        private List<Point> snakeSegments = new List<Point>();
+        readonly List<Point> snakeSegments = new List<Point>();
         private bool isPaused = false;
 
         public MainWindow()
@@ -182,57 +182,5 @@ namespace SnakeWPF
             MessageBoxResult result = MessageBox.Show("Biztosan ki akar lépni?", "Megerősítés", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
             if (result == MessageBoxResult.Yes) Application.Current.Shutdown();
         }
-    }
-
-    public class Food
-    {
-        private readonly Canvas gameCanvas;
-        private Random random = new Random();
-        private readonly int foodSize = 20;
-        private Ellipse foodPiece;
-        private List<Point> snakeSegments;
-
-        public Food(Canvas canvas, List<Point> snakeSegments)
-        {
-            gameCanvas = canvas;
-            this.snakeSegments = snakeSegments;
-            GenerateFood();
-        }
-
-        private void GenerateFood()
-        {
-            double left = random.Next(0, (int)(gameCanvas.ActualWidth / foodSize)) * foodSize;
-            double top = random.Next(0, (int)(gameCanvas.ActualHeight / foodSize)) * foodSize;
-
-
-            foodPiece = new Ellipse
-            {
-                Width = foodSize,
-                Height = foodSize,
-                Fill = Brushes.Red
-            };
-
-            gameCanvas.Children.Add(foodPiece);
-            Canvas.SetLeft(foodPiece, left);
-            Canvas.SetTop(foodPiece, top);
-        }
-
-        public bool Eat(Point snakeHeadPosition)
-        {
-            double foodLeft = Canvas.GetLeft(foodPiece);
-            double foodTop = Canvas.GetTop(foodPiece);
-
-            if (snakeHeadPosition.X == foodLeft && snakeHeadPosition.Y == foodTop)
-            {
-                gameCanvas.Children.Remove(foodPiece);
-                GenerateFood();
-
-                Point newSegmentPositon = new Point(snakeHeadPosition.X, snakeHeadPosition.Y);
-                snakeSegments.Add(newSegmentPositon);
-                return true;
-            }
-            return false;
-        }
-
     }
 }
